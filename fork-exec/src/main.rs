c98daecea1bae5fc68f343c0753bdd5ac1cbec45
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2021-10-02 13:58:42 +0100

    ptrace options

--- fork-exec/src/main.rs
+++ fork-exec/src/main.rs
@@ -4,6 +4,7 @@ use std::io;
 use std::vec::Vec;
 use core::ptr;
 
+#[allow(dead_code)]
 mod libc {
     use std::os::raw::{c_char,c_int};
 
@@ -11,6 +12,7 @@ mod libc {
     pub(crate) type pid_t = c_int;
 
     // signals
+    pub(crate) const SIGKILL: c_int = 9;
     pub(crate) const SIGSTOP: c_int = 19;
 
     // ptrace commands
@@ -21,10 +23,32 @@ mod libc {
     pub(crate) const PTRACE_DETACH: i32 = 17;
     pub(crate) const PTRACE_SEIZE: i32 = 0x4206;
     pub(crate) const PTRACE_INTERRUPT: i32 = 0x4207;
+    pub(crate) const PTRACE_SETOPTIONS: i32 = 0x4200;
+
+    pub(crate) const PTRACE_EVENT_FORK: i32 = 1;
+    pub(crate) const PTRACE_EVENT_VFORK: i32 = 2;
+    pub(crate) const PTRACE_EVENT_CLONE: i32 = 3;
+    pub(crate) const PTRACE_EVENT_EXEC: i32 = 4;
+    pub(crate) const PTRACE_EVENT_VFORK_DONE: i32 = 5;
+    pub(crate) const PTRACE_EVENT_EXIT: i32 = 6;
+    pub(crate) const PTRACE_EVENT_SECCOMP: i32 = 7;
+    pub(crate) const PTRACE_EVENT_STOP: i32 = 128;
+
+    pub(crate) const PTRACE_O_TRACESYSGOOD: usize = 1;
+    pub(crate) const PTRACE_O_TRACEFORK: usize = 1 << PTRACE_EVENT_FORK;
+    pub(crate) const PTRACE_O_TRACEVFORK: usize = 1 << PTRACE_EVENT_VFORK;
+    pub(crate) const PTRACE_O_TRACECLONE: usize = 1 << PTRACE_EVENT_CLONE;
+    pub(crate) const PTRACE_O_TRACEEXEC: usize = 1 << PTRACE_EVENT_EXEC;
+    pub(crate) const PTRACE_O_TRACEVFORKDONE: usize = 1 << PTRACE_EVENT_VFORK_DONE;
+    pub(crate) const PTRACE_O_TRACEEXIT: usize = 1 << PTRACE_EVENT_EXIT;
+    pub(crate) const PTRACE_O_TRACESECCOMP: usize = 1 << PTRACE_EVENT_SECCOMP;
+    pub(crate) const PTRACE_O_EXITKILL: usize = 1 << 20;
+    pub(crate) const PTRACE_O_SUSPEND_SECCOMP: usize = 1 << 21;
 
 
     extern "C" {
-        pub(crate) fn ptrace(request: c_int, pid: pid_t, addr: *mut u8, data: *mut u8) -> i64;
+        pub(crate) fn
+            ptrace(request: c_int, pid: pid_t, addr: usize, data: usize) -> i64;
         pub(crate) fn waitpid(pid: pid_t, status: *mut c_int, options: c_int) -> c_int;
         pub(crate) fn fork() -> c_int;
         pub(crate) fn execv(path: *const c_char, argv: *const *const c_char) -> c_int;
@@ -32,11 +56,15 @@ mod libc {
     }
 }
 
+/// Result of forking the process.
 enum ForkRes {
+    /// Returned in the parent, with the chile pid.
     Parent(i32),
+    /// Returned in the child.
     Child,
 }
 
+/// Fork the process.
 fn fork() -> io::Result<ForkRes> {
     let res = unsafe { libc::fork() };
 
@@ -74,6 +102,7 @@ fn execv(path: &str, args: &[&str]) -> io::Result<()> {
     // turn the Vec<*const c_char> into a *const *const c_char
     let argv = argv.as_ptr();
 
+    // finally call execv
     let _ = unsafe { libc::execv(path, argv) };
 
     // ok to just have this return path, if the exec succeeded the program will
@@ -90,22 +119,24 @@ fn waitpid(pid: i32) -> io::Result<()> {
     Ok(())
 }
 
-fn debugger(pid: i32) -> io::Result<()> {
-    println!("parent end, child pid: {}", pid);
+fn ptrace_setoptions(pid: i32, opts: usize) -> io::Result<()> {
+    let res = unsafe { libc::ptrace(libc::PTRACE_SETOPTIONS, pid, 0, opts) };
+    if res < 0 {
+        return Err(io::Error::last_os_error())
+    }
+    Ok(())
+}
 
-    // wait for child to exist
+fn debugger(pid: i32) -> io::Result<()> {
+    println!("parent, child pid: {}", pid);
+
+    // wait for child to ptrace(traceme)
     waitpid(pid)?;
 
-    // sleep for 2s, just to do something
-    std::thread::sleep(std::time::Duration::from_millis(2000));
+    ptrace_setoptions(pid, libc::PTRACE_O_EXITKILL | libc::PTRACE_O_TRACEEXEC)?;
 
     println!("going to cont");
-    let res = unsafe { libc::ptrace(
-        libc::PTRACE_CONT,
-        pid,
-        std::ptr::null_mut(),
-        std::ptr::null_mut())
-    };
+    let res = unsafe { libc::ptrace( libc::PTRACE_CONT, pid, 0, 0) };
 
     if res < 0 {
         return Err(io::Error::last_os_error())
@@ -126,11 +157,16 @@ fn debugger(pid: i32) -> io::Result<()> {
 
     std::thread::sleep(std::time::Duration::from_millis(2000));
 
+    println!("going to cont");
+    let res = unsafe { libc::ptrace( libc::PTRACE_CONT, pid, 0, 0) };
+
+    std::thread::sleep(std::time::Duration::from_millis(2000));
+
     Ok(())
 }
 
 fn debugee() -> io::Result<()> {
-    unsafe { libc::ptrace(libc::PTRACE_TRACEME, 0, ptr::null_mut(), ptr::null_mut()); }
+    unsafe { libc::ptrace(libc::PTRACE_TRACEME, 0, 0, 0); }
     execv("/home/adam/src/debug/test/main", &["test"])
 }
 
